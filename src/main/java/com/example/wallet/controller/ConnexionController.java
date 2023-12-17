f6a3478ee10f309d4778002e0d5fbf0617cf448d
Rayan2110 <rayan.kallal54@yahoo.fr>
2023-12-17 13:10:12 +0100

    modif du jour

--- src/main/java/com/example/wallet/controller/ConnexionController.java
+++ src/main/java/com/example/wallet/controller/ConnexionController.java
@@ -20,7 +20,7 @@ public class ConnexionController {
     @FXML
     public TextField idField;
     @FXML
-    public Label errror_label;
+    public Label error_label;
 
 
     @FXML
@@ -34,7 +34,14 @@ public class ConnexionController {
     @FXML //liaison avec le FXML
     public void onConnexionButtonClick(ActionEvent actionEvent) throws IOException {
         if(passwordField.getText().equals("Rayan2110@") && idField.getText().equals("Rayan2110")){
-            errror_label.setText("Connexion réussit!");
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("home-view.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 1024, 768); // standard de base
+            stage.setScene(scene);
+            stage.show();
+            error_label.setText("Connexion réussit!");
+        }
+        else {
+            error_label.setText("Erreur! Veuillez Recommencez.");
         }
     }
 }
--- src/main/java/com/example/wallet/controller/InscriptionController.java
+++ src/main/java/com/example/wallet/controller/InscriptionController.java
@@ -5,12 +5,18 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 
 import java.io.IOException;
 
 import static com.example.wallet.HelloApplication.stage;
 
+
+
 public class InscriptionController {
+    @FXML
+    public Label descriptionLabel;
+
     @FXML
     public void onReturnButtonClick(ActionEvent actionEvent) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
@@ -18,4 +24,14 @@ public class InscriptionController {
         stage.setScene(scene);
         stage.show();
     }
+
+    public void onInscriptionButtonClick(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1024, 768); // standard de base
+        descriptionLabel.setText("Inscription réussit !");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
 }
