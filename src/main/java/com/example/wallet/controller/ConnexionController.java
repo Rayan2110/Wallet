e1d21abf1cec9c3a478ce9ef412b9f56d4952540
Rayan2110 <rayan.kallal54@yahoo.fr>
2023-12-17 11:22:40 +0100

    modif

--- src/main/java/com/example/wallet/controller/ConnexionController.java
+++ src/main/java/com/example/wallet/controller/ConnexionController.java
@@ -5,6 +5,9 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 import java.io.IOException;
 
@@ -13,10 +16,25 @@ import static com.example.wallet.HelloApplication.stage;
 public class ConnexionController {
 
     @FXML
+    public PasswordField passwordField;
+    @FXML
+    public TextField idField;
+    @FXML
+    public Label errror_label;
+
+
+    @FXML
     public void onReturnButtonClick(ActionEvent actionEvent) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1024, 768); // standard de base
         stage.setScene(scene);
         stage.show();
+    }
+
+    @FXML //liaison avec le FXML
+    public void onConnexionButtonClick(ActionEvent actionEvent) throws IOException {
+        if(passwordField.getText().equals("Rayan2110@") && idField.getText().equals("Rayan2110")){
+            errror_label.setText("Connexion réussit!");
+        }
     }
 }
